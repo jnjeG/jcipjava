c91cb72b450e74343e3187ee303010ed9947dccf
jnjeG <qchycjj@126.com>
2015-06-09 17:50:34 +0800

    add some comment

--- src/net/jcip/examples/Puzzle.java
+++ src/net/jcip/examples/Puzzle.java
@@ -8,8 +8,17 @@ import java.util.*;
  * Abstraction for puzzles like the 'sliding blocks puzzle'
  *
  * @author Brian Goetz and Tim Peierls
+ * 
+ * P may represent as one box or more boxes.
+ * M may represent as one box's movment or many boxes' movment
+ * @author chenjunjie
  */
 public interface Puzzle <P, M> {
+  /**
+   * If P represent one box's position,then this method may initial one position.
+   * Otherwise,if P represent a series of nodes,then this method should initial all boxes' position.
+   * @return
+   */
     P initialPosition();
 
     boolean isGoal(P position);
